78f7abb10621ec272840e5696f41586b6bcc3b50
alfiecri <jediknightalfie@gmail.com>
2020-11-09 19:34:42 +0800

    Initial commit

---
+++ Assets/Script.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Script : MonoBehaviour
+{
+    float space = 0;
+    public GameObject spaceBarCounter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spaceBarCounter.GetComponent<Text>().text = "SpaceBar: " + space;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            spaceBarCounter.GetComponent<Text>().text = "SpaceBar: " + ++space;
+            MonoBehaviour.print("spaceBarCounter: " + space);
+        }
+    }
+}
